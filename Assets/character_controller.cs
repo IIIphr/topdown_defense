044f9a45435aff536fdce2cdf2925667aa5c1ded
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-07-05 19:36:12 +0430

    Added character controller, animation and sprite

---
+++ Assets/character_controller.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_controller : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool flag = false;
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            flag = true;
+            rb.velocity = new Vector3(rb.velocity.x, 1, 0);
+            animator.SetBool("is_walking_up", true);
+            animator.SetBool("is_walking_down", false);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            flag = true;
+            rb.velocity = new Vector3(rb.velocity.x, -1, 0);
+            animator.SetBool("is_walking_up", false);
+            animator.SetBool("is_walking_down", true);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            flag = true;
+            rb.velocity = new Vector3(1, rb.velocity.y, 0);
+            animator.SetBool("is_walking_right", true);
+            animator.SetBool("is_walking_left", false);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            flag = true;
+            rb.velocity = new Vector3(-1, rb.velocity.y, 0);
+            animator.SetBool("is_walking_right", false);
+            animator.SetBool("is_walking_left", true);
+        }
+        if (!flag)
+        {
+            rb.velocity = Vector3.zero;
+            animator.SetBool("is_walking_up", false);
+            animator.SetBool("is_walking_down", false);
+            animator.SetBool("is_walking_right", false);
+            animator.SetBool("is_walking_left", false);
+        }
+    }
+}
