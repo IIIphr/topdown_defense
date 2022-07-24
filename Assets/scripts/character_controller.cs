ba8d1da2a136cf87976e80b0a0c7598ec0a92167
Sepehr <62986692+IIIphr@users.noreply.github.com>
2022-07-24 10:37:50 +0430

    Rewrote animation using CrossFade, camera follows the player

---
+++ Assets/scripts/character_controller.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_controller : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float movement_speed = 1f;
+    [SerializeField] Animator animator;
+    [SerializeField] float camera_distance = 5f;
+    string anim_state;
+    int facing_dir = 0;
+    // 0 : down
+    // 1 : up
+    // 2 : right
+    // 3 : left
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim_state = "idle";
+        animator.CrossFade("idle", 0, 0);
+    }
+
+    string dir_to_str()
+    {
+        return
+            facing_dir == 0 ? "down" :
+            facing_dir == 1 ? "up" :
+            facing_dir == 2 ? "right" :
+            "left";
+    }
+
+    int str_to_dir(string str)
+    {
+        return
+            str == "down" ? 0 :
+            str == "up" ? 1 :
+            str == "right" ? 2 :
+            3;
+    }
+
+   void anim_change(string state)
+    {
+        bool is_slashing = Input.GetAxis("Jump") > 0;
+        if (state == "idle")
+        {
+            if (is_slashing)
+            {
+                state = "slash " + dir_to_str();
+            }
+            else
+            {
+                state = dir_to_str() + " idle";
+            }
+            if (state != anim_state)
+            {
+                animator.CrossFade(state, 0, 0);
+                anim_state = state;
+            }
+        }
+        else if(anim_state != state || is_slashing)
+        {
+            if (is_slashing)
+            {
+                if(anim_state != "slash " + state)
+                {
+                    animator.CrossFade("slash " + state, 0, 0);
+                    anim_state = "slash " + state;
+                }
+            }
+            else
+            {
+                animator.CrossFade(state, 0, 0);
+                anim_state = state;
+            }
+            facing_dir = str_to_dir(state);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move_vector = new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"),
+            0);
+        rb.velocity = move_vector * movement_speed;
+        if(move_vector.x > 0)
+        {
+            anim_change("right");
+        }
+        else if(move_vector.x < 0)
+        {
+            anim_change("left");
+        }
+        else if (move_vector.y > 0)
+        {
+            anim_change("up");
+        }
+        else if (move_vector.y < 0)
+        {
+            anim_change("down");
+        }
+        else
+        {
+            anim_change("idle");
+        }
+        Camera.main.transform.position = new Vector3(
+            transform.position.x, transform.position.y, -1 * camera_distance);
+    }
+}
